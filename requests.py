6ff4012bc2bcbd5e7169f7dd4341989321aad316
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2024-06-18 14:12:38 -0300

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,7 +1,10 @@
+pip install streamlit
+pip install pandas
+pip install openpyx1
+streamlit run app.py 
 import pandas == 2.2.2
 import matplotlib.pyplot == 3.9.0
 import plotly.graph_objs == 5.22.0
 import plotly.express == 5.22.0
 import folium == 0.17.0
-!pip install streamlit plotly pandas openpyxl
-streamlit run app.py 
+
