78469a51bdefff19f2c9d3b010f22b9ce6aff4e9
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2024-06-18 14:56:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,18 +32,18 @@ dados_consolid = pd.pivot_table(dados_filtrados, values='Detecção', index=['La
 dados_consolid.columns = ['Latitude', 'Longitude', 'Municipio', 'Ponto de Coleta', 'CRS', 'Detecções_Total', 'Detecções_Contagem']
 
 # Crie o mapa
-#mapa_folium = folium.Map(location=[dados_consolid["Latitude"].mean(), dados_consolid["Longitude"].mean()], zoom_start=5)
+mapa_folium = folium.Map(location=[dados_consolid["Latitude"].mean(), dados_consolid["Longitude"].mean()], zoom_start=5)
 
 # Adicione marcadores
-#for i, row in dados_consolid.iterrows():
-#    folium.Marker([row["Latitude"], row["Longitude"]]).add_to(mapa_folium)
+for i, row in dados_consolid.iterrows():
+    folium.Marker([row["Latitude"], row["Longitude"]]).add_to(mapa_folium)
 
 # Adicione camadas (opcional)
 # Adicione uma camada de azulejos com diferentes estilos
-#folium.TileLayer('Stamen Toner', attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.').add_to(mapa_folium)
+folium.TileLayer('Stamen Toner', attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.').add_to(mapa_folium)
 
 # Mostre o mapa
-#folium_static(mapa_folium)
+folium_static(mapa_folium)
 
 
 # Configurar o token do Mapbox
