900d80bc37614d3285087943a0759fb6eb177f80
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2024-06-18 14:33:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,20 +31,22 @@ dados_consolid.columns = ['Latitude', 'Longitude', 'Municipio', 'Ponto de Coleta
 
 # Mostre o mapa
 #folium_static(mapa_folium)
+
+
 # Configurar o token do Mapbox
 token = 'pk.eyJ1IjoiYW5kcmUtamFyZW5rb3ciLCJhIjoiY2xkdzZ2eDdxMDRmMzN1bnV6MnlpNnNweSJ9.4_9fi6bcTxgy5mGaTmE4Pw'
 px.set_mapbox_access_token(token)
 
 # Crie o mapa
 mapa_px = px.scatter_mapbox(
-    data_frame = dados_consolid,
+    data_frame=dados_consolid,
     lat="Latitude",
     lon="Longitude",
     title="Mapa de Pontos de Detecção de Agrotóxicos no RS",
     zoom=6,
-    color = "sum",
-    size = "count",
-    height = 800,
+    color="Detecções_Total",  # Use a coluna correta
+    size="Detecções_Contagem",  # Use a coluna correta
+    height=800,
     color_continuous_scale=px.colors.sequential.Sunsetdark,
     size_max=15,
     mapbox_style="open-street-map"
@@ -53,5 +55,5 @@ mapa_px = px.scatter_mapbox(
 # Adicione uma legenda
 mapa_px.update_layout(legend_title="Detecção de Agrotóxicos no RS")
 
-# Mostre o mapa
-mapa_px.show()
+# Mostre o mapa no Streamlit
+st.plotly_chart(mapa_px)
