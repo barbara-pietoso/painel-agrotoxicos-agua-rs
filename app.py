b0428acfdb6b6a3d3f9c35c65f38c2510766d481
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-06-19 13:10:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ dados = pd.read_excel('https://docs.google.com/spreadsheets/d/e/2PACX-1vRR1E1xhX
 
 # Filtrar as linhas com valores válidos de latitude e longitude
 dados_filtrados = dados.dropna(subset=["Latitude", "Longitude"])
+dados_filtrados['Parametros detectados'].fillna('Verificando', inplace=True)
 
 # Consolidar os dados
 dados_consolid = pd.pivot_table(dados_filtrados, values='Detecção', index=['Latitude','Longitude', 'Municipio', 'Ponto de Coleta', 'CRS', 'Parametros detectados'],
@@ -36,12 +37,12 @@ dados_consolid.columns = ['Latitude', 'Longitude', 'Municipio', 'Ponto de Coleta
 mapa_folium = folium.Map(location=[dados_consolid["Latitude"].mean(), dados_consolid["Longitude"].mean()], zoom_start=5)
 
 # Adicione marcadores
-for i, row in dados_consolid.iterrows():
-    folium.Marker([row["Latitude"], row["Longitude"]]).add_to(mapa_folium)
+#for i, row in dados_consolid.iterrows():
+#    folium.Marker([row["Latitude"], row["Longitude"]]).add_to(mapa_folium)
 
 # Adicione camadas (opcional)
 # Adicione uma camada de azulejos com diferentes estilos
-folium.TileLayer('Stamen Toner', attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.').add_to(mapa_folium)
+#folium.TileLayer('Stamen Toner', attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.').add_to(mapa_folium)
 
 # Mostre o mapa
 #folium_static(mapa_folium)
