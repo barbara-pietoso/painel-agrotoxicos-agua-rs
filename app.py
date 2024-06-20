3e5a31c191ec575e2099ffffbd6cec1d806fb2c5
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2024-06-20 10:05:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ with col4:
         px.set_mapbox_access_token(token)
 
     # Definindo o centro do mapa
-            center_lat = -29.5,  # Latitude central aproximada do Rio Grande do Sul
+            center_lat = -29.5  # Latitude central aproximada do Rio Grande do Sul
             center_lon = -53.5  # Longitude central aproximada do Rio Grande do Sul
            
         # Crie o mapa
