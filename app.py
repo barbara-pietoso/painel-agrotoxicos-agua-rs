86b7211f855606c84c9d209daf2f8d73efd945d5
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-06-25 17:24:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ dados_filtrados = dados_detec.dropna(subset=["Latitude", "Longitude"])
 dados_filtrados['Parametros detectados'].fillna('Verificando', inplace=True)
 
 dados_consolid = pd.pivot_table(dados_filtrados, values='Detecção', index=['Latitude','Longitude', 'Municipio', 'Ponto de Coleta', 'CRS', 'Parametros detectados'], aggfunc=['sum', 'count']).reset_index()
-st.dataframe(dados_consolid)
+st.dataframe(dados_consolid.dropna())
 dados_consolid = dados_consolid.droplevel(0).rename({'sum':'Detecções_Total', 'count':'Detecções_Contagem'} )
 
 def processar_parametros(parametros):
