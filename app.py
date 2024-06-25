a90555bc2b224f1e54f8bb3d9f0c9e7cb62eeb58
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2024-06-25 13:34:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ with col4:
             size="Detecções_Contagem",  # Use a coluna correta
             height=800,
             width=800,
-            color_continuous_scale=px.colors.sequential.Oranges,
+            color_discrete_sequence=["orange"],
             size_max=15, #tamanho maximo dos pontos
             mapbox_style="open-street-map"
         )
