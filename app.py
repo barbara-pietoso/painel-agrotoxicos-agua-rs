eabae1313259ebb2ab4633cc871b55bc2139f0fc
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2024-07-02 16:09:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,12 +52,12 @@ with col10:
         dados_filtrados['Parametros detectados'].fillna('Verificando', inplace=True)
         
         dados_consolid = pd.pivot_table(dados_filtrados, values='Detecção', index=['Latitude','Longitude', 'Municipio', 'Ponto de Coleta',
-                                                                                   'CRS', 'Parametros detectados'], aggfunc=['sum', 'count']).reset_index()
+                                                                                   'CRS', 'Parametros detectados', 'Zona'], aggfunc=['sum', 'count']).reset_index()
         
         try:
-            dados_consolid.columns = ['Latitude', 'Longitude', 'Municipio', 'Ponto de Coleta', 'CRS','Parametros detectados', 'Detecções_Total', 'Detecções_Contagem', ]
+            dados_consolid.columns = ['Latitude', 'Longitude', 'Municipio', 'Ponto de Coleta', 'CRS','Parametros detectados', 'Zona' 'Detecções_Total', 'Detecções_Contagem', ]
         except:
-            dados_consolid.columns = ['Latitude', 'Longitude', 'Municipio', 'Ponto de Coleta', 'CRS','Parametros detectados']
+            dados_consolid.columns = ['Latitude', 'Longitude', 'Municipio', 'Ponto de Coleta', 'CRS','Parametros detectados', 'Zona']
         
         def processar_parametros(parametros):
             for parametro in parametros.split(','):
