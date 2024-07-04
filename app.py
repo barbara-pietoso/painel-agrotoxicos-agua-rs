94bc6986602b23a8ad6a3d45f63ecd58a1db94ce
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-04 15:08:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ with col10:
                 dados_detec = dados[dados['Detecção']>0].reset_index(drop=True)
                 
                 # Filtrar as linhas com valores válidos de latitude e longitude
-                dados_filtrados = dados_detec.dropna(subset=["Latitude", "Longitude"])
+                dados_filtrados = dados_detec.dropna(subset=["Latitude", "Longitude"]).reset_index(drop=True)
                 dados_filtrados['Parametros detectados'].fillna('Verificando', inplace=True)
                 
                 dados_consolid = pd.pivot_table(dados_filtrados, values='Detecção', index=['Latitude','Longitude', 'Municipio', 'Ponto de Coleta',
