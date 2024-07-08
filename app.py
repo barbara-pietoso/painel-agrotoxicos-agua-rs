671820f0a68503f2dd3f2604d95908f1badf6898
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-08 15:22:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ with col10:
 		    lista_crs_selectbox = sorted(dados['CRS'].unique())
 		    lista_crs_selectbox.insert(0,'Todas')
 		    CRS = st.selectbox("Selecione a CRS", lista_crs_selectbox, index=0, placeholder="Nenhuma CRS selecionada")
+		    lista_munipios_crs = dados_municipios['Município']
 		    if CRS != 'Todas':
 			    dados = dados[dados['CRS']==CRS]
 			    lista_munipios_crs = dados_municipios[dados_municipios['CRS']==CRS]['Município']
