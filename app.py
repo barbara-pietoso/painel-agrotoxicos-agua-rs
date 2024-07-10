cb7cdd30acbaa6fd78e1476cd5eb8a51c848b057
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-10 17:53:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -398,22 +398,72 @@ for data in grafico_mun_detec.data:
 st.plotly_chart(grafico_mun_detec, theme = None)
 
 # teste echarts
-options = {
-    "xAxis": {
-        "type": "category",
-        "boundaryGap": False,
-        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    "yAxis": {"type": "value"},
-    "series": [
-        {
-            "data": [820, 932, 901, 934, 1290, 1330, 1320],
-            "type": "line",
-            "areaStyle": {},
-        }
-    ],
+with open("./data/life-expectancy-table.json") as f:
+    raw_data = json.load(f)
+countries = [
+    "Finland",
+    "France",
+    "Germany",
+    "Iceland",
+    "Norway",
+    "Poland",
+    "Russia",
+    "United Kingdom",
+]
+
+datasetWithFilters = [
+    {
+        "id": f"dataset_{country}",
+        "fromDatasetId": "dataset_raw",
+        "transform": {
+            "type": "filter",
+            "config": {
+                "and": [
+                    {"dimension": "Year", "gte": 1950},
+                    {"dimension": "Country", "=": country},
+                ]
+            },
+        },
+    }
+    for country in countries
+]
+
+seriesList = [
+    {
+        "type": "line",
+        "datasetId": f"dataset_{country}",
+        "showSymbol": False,
+        "name": country,
+        "endLabel": {
+            "show": True,
+            "formatter": JsCode(
+                "function (params) { return params.value[3] + ': ' + params.value[0];}"
+            ).js_code,
+        },
+        "labelLayout": {"moveOverlap": "shiftY"},
+        "emphasis": {"focus": "series"},
+        "encode": {
+            "x": "Year",
+            "y": "Income",
+            "label": ["Country", "Income"],
+            "itemName": "Year",
+            "tooltip": ["Income"],
+        },
+    }
+    for country in countries
+]
+
+option = {
+    "animationDuration": 10000,
+    "dataset": [{"id": "dataset_raw", "source": raw_data}] + datasetWithFilters,
+    "title": {"text": "Income in Europe since 1950"},
+    "tooltip": {"order": "valueDesc", "trigger": "axis"},
+    "xAxis": {"type": "category", "nameLocation": "middle"},
+    "yAxis": {"name": "Income"},
+    "grid": {"right": 140},
+    "series": seriesList,
 }
-st_echarts(options=options)
+st_echarts(options=option, height="600px")
 
 # Funções de layout
 
