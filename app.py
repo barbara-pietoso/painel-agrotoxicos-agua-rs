050ea75a52f69f764facc6cd39ace50c03c0e5b9
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-10 16:54:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -142,25 +142,25 @@ with col10:
 with col6:
     total_amostras = len(dados)
     with st.container(border=True):
-        stx.metric("Total de Amostras Coletadas", total_amostras)
+        stx.metric(label="Total de Amostras Coletadas", value = total_amostras)
 
 # Quantas detecções
 with col7:
     total_deteccoes = dados_detec['Detecção'].sum()
     with st.container(border=True):
-        stx.metric("Total de Detecções", total_deteccoes)
+        stx.metric(label="Total de Detecções", value = total_deteccoes)
 
 # Quantos municípios com detecção
 with col8:
     municipios_com_detec = dados_detec['Municipio'].nunique()
     with st.container(border=True):
-        stx.metric("Municípios com Detecção", municipios_com_detec)
+        stx.metric(label="Municípios com Detecção", value = municipios_com_detec)
 
 # Quantos municípios houve coleta
 with col9:
     municipios_com_coleta = dados['Municipio'].nunique()
     with st.container(border=True):
-        stx.metric("Municípios com Coleta", municipios_com_coleta)
+        stx.metric(label="Municípios com Coleta", value = municipios_com_coleta)
 
 col5, col4 = st.columns([4, 4]) 
     
