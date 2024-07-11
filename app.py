ee26da6408fa58aa134c4ebab3f3f16be2cde1a6
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-11 17:19:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -398,15 +398,60 @@ for data in grafico_mun_detec.data:
 st.plotly_chart(grafico_mun_detec, theme = None)
 
 # teste echarts
-options = {
+option = {
+    "tooltip": {
+        "trigger": "axis",
+        "axisPointer": {
+            "type": "cross"
+        }
+    },
     "xAxis": {
         "type": "category",
-        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        "data": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+        "axisPointer": {
+            "type": "shadow"
+        }
     },
-    "yAxis": {"type": "value"},
-    "series": [{"data": [120, 200, 150, 80, 70, 110, 130], "type": "bar"}],
+    "yAxis": [
+        {
+            "type": "value",
+            "name": "Total de amostras",
+            "min": 0,
+            "max": 250,
+            "interval": 50,
+            "axisLabel": {
+                "formatter": "{value}"
+            }
+        },
+        {
+            "type": "value",
+            "name": "Percentual de detecção",
+            "min": 0,
+            "max": 25,
+            "interval": 5,
+            "axisLabel": {
+                "formatter": "{value} %"
+            }
+        }
+    ],
+    "series": [
+        {
+            "name": "Total de amostras",
+            "type": "bar",
+            "data": [18, 64, 143, 82, 70, 64, 55, 85, 115, 167, 174, 201]
+        },
+        {
+            "name": "Percentual de detecção",
+            "type": "line",
+            "yAxisIndex": 1,
+            "data": [0,15.6, 15.4, 8.6, 10, 6.2, 7.3, 2.4, 6.1, 7.8, 10.9, 8]
+        }
+    ]
 }
-st_echarts(options=options, height="500px")
+
+st_echarts(
+    options=option, height="400px",
+)
 
 
 # Funções de layout
