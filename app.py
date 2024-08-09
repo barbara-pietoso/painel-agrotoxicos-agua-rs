a3057beaa1f20a4c84567740a455a812102c9ffa
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-08-09 16:10:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -341,7 +341,7 @@ with col5:
                 side='right'
             ),
             height=300,
-            legend=dict(x=0.4, y=1.2, bgcolor='rgba(255, 255, 255, 0)')
+            legend=dict(x=0.4, y=1.5, bgcolor='rgba(255, 255, 255, 0)')
         )
 
     
